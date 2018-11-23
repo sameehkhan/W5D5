948efe2b90a7377121de9dda75f1a77df1077024
sameehkhan <sameehkhan1@gmail.com>
2018-11-23 15:05:33 -0800

    Create incremental clock function

--- callbacks.js
+++ callbacks.js
@@ -2,8 +2,47 @@ class Clock {
   constructor() {
     const start = new Date();
 
-    console.log(start.getHours());
+    this.hours = start.getHours();
+    this.min = start.getMinutes();
+    this.sec = start.getSeconds();
+
+    // let clock = new Clock();
+    this.printTime();
+
+    setInterval(this._tick.bind(this), 0.0001);
+  }
+  printTime() {
+    console.log(`${this.hours}:${this.min}:${this.sec}`);
+
+  }
+
+  _tick() {
+
+    if ( this.sec < 59){
+      this.sec += 1;
+    } else {
+        if (this.min < 59){
+          this.sec = 0;
+          this.min += 1;
+        } else {
+          if (this.hours < 23){
+            this.hours += 1;
+            this.min = 0;
+            this.sec = 0;
+          } else {
+            this.hours = 0;
+            this.min = 0;
+            this.sec = 0;
+          }
+        }
+      }
+    this.printTime();
   }
 }
+
+// find current time
+// parse time, increment
+// conditional for increments
+// print time at end
 
 const clock = new Clock();
