5f412b3f2517ec314767ab9ffdd7d25b55bcc72e
sameehkhan <sameehkhan1@gmail.com>
2018-11-23 16:19:04 -0800

    Correct reader close location for addnumber

--- addNumbers.js
+++ addNumbers.js
@@ -13,17 +13,17 @@ function addNumbers (sum = 0, numsLeft, completionCallback) {
       reader.question("What is your number?", function (answer) {
         sum += parseInt(answer);
         numsLeft -=1;
+        console.log("Partial sum: " + sum);
         addNumbers(sum, numsLeft, completionCallback);
       });
     } else {
-      reader.close();
 
-      return sum;
+      completionCallback(sum);
 
     }
-    completionCallback(sum);
 }
 
 const addNum = new addNumbers(0, 3, (el) => {
   console.log(`Total sum: ${el}`);
+  reader.close();
 });
